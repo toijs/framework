d9e17083f8c5c32bee5e3f273ce28a7da502d621
hblab-toitl <toitl@hblab.vn>
2026-08-13 12:49:31 +0700

    feat: add event register to modular

--- modular/src/launcher/services/shell.service.ts
+++ modular/src/launcher/services/shell.service.ts
@@ -1,6 +1,6 @@
 import { Metadata } from "../../metadata";
 import { Task, TaskContext } from "../../task";
-import { TASK_ROOT_READY } from "../constants";
+import { TASK_ROOT_READY, TASK_ROOT_REGISTER } from "../constants";
 
 export class Shell {
   private instance: unknown;
@@ -22,7 +22,16 @@ export class Shell {
   async create(callback: () => unknown) {
     const instance = await callback();
     this.instance = instance;
+    await this.task.invoke(TASK_ROOT_REGISTER, instance);
     await this.task.invoke(TASK_ROOT_READY, instance);
+  }
+
+  /**
+   * Subscribe to the app instance event
+   * @param callback - The callback to subscribe to the app instance event
+   */
+  register(callback: (context: TaskContext) => void) {
+    return this.task.subscribe(TASK_ROOT_REGISTER, callback);
   }
 
   /**
